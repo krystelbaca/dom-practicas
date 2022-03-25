fc747da410382a3b8d6a4fd6b6555a1bc2db601d
Krystel Baca <krystelbaca@Krystels-MacBook-Pro.local>
2022-03-24 19:31:00 -0600

    solving bug with counter and decrement button

--- contador/app.js
+++ contador/app.js
@@ -2,10 +2,24 @@
 let count = 0
 
 let value = document.getElementById('contador')
-const btnIncrement = document.getElementsByClassName('.increment')
 
 
 function increment() {
-    value = count++
-    document.getElementById('contador').innerHTML = value
+    // al mandarse llamar la función con el click inmediatamente aumentamos el contador
+    // y luego asignamos
+    count++
+    value.textContent = count
 }
+
+function decrement() {
+    count--
+    value.textContent = count
+}
+
+
+/**
+ * textContent
+ * Get all the textual content of an <ul> element with id="myList": https://www.w3schools.com/jsref/prop_node_textcontent.asp
+ejemplo: 
+let text = document.getElementById("myList").textContent;
+ */
